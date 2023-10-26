54b18529a17fb071fafb0bbbd556d9ad8d6a03ff
Jared Karpiak <jakarpiak@gmail.com>
2023-10-25 18:56:29 -0600

    Finished backend and added new react front end project

--- back_end/sanity.config.js
+++ back_end/sanity.config.js
@@ -2,9 +2,12 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import user from './schemas/user'
-
+import comment from './schemas/comment'
+import pin from './schemas/pin'
+import postedBy from './schemas/postedBy'
+import save from './schemas/save'
 const schemaTypes = [
-  user
+  user, comment, pin, postedBy, save
 ]
 export default defineConfig({
   name: 'default',
